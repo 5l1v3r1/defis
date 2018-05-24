e94e932bc17849a605214f1b169522c433196122
xhermit <xhermitone@gmail.com>
2018-05-24 13:28:30 +0700

    отладка

--- ic/Scripts/debug.py
+++ ic/Scripts/debug.py
@@ -1,2 +1,10 @@
-import winpdb
-winpdb.main()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    import winpdb
+    winpdb.main()
+except ImportError:
+    print(u'WinPDB not installed')
+    print(u'For installition use:')
+    print(u'\tsudo apt install winpdb')
